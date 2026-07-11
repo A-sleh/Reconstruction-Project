ac00ec74aca37a9da9176f44a996ee6ce4f4348b
A-sleh <businessabdo9@gmail.com>
2026-07-11 14:25:13 +0300

    feat(order) : linke the mark as full filled order

--- src/features/resource-providor/order-details/components/MarkFullyDelivered.tsx
+++ src/features/resource-providor/order-details/components/MarkFullyDelivered.tsx
@@ -1,7 +1,8 @@
+import { OrderStatus } from "@/features/orders/api/types";
 import { Switch } from "@/components/ui/switch";
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { useMarkOrderFullyDelivered } from "../api/actions";
+import { useMarkAsReceived } from "@/features/orders/api/actions";
 
 interface MarkFullyDeliveredProps {
   id: number | string;
@@ -9,7 +10,7 @@ interface MarkFullyDeliveredProps {
   delivered: number;
   fullyDelivered: boolean;
   totalInvoiced: number;
-  status: string;
+  status: OrderStatus;
 }
 
 const MarkFullyDelivered: React.FC<MarkFullyDeliveredProps> = ({
@@ -17,12 +18,12 @@ const MarkFullyDelivered: React.FC<MarkFullyDeliveredProps> = ({
   fullyDelivered = false,
   total = 0,
   totalInvoiced = 0,
-  status = "",
+  status = "" as OrderStatus,
   id = "",
 }) => {
   const { t } = useTranslation();
   const { mutate: markFullyDelivered, isPending } =
-    useMarkOrderFullyDelivered();
+    useMarkAsReceived();
 
   return (
     <div className="mt-6 rounded-xl border border-gray-300 bg-card p-4 flex items-center justify-between flex-wrap gap-3 bg-white">
@@ -46,11 +47,13 @@ const MarkFullyDelivered: React.FC<MarkFullyDeliveredProps> = ({
         )}
         <Switch
           checked={fullyDelivered}
-          // The control element disables during loading, if rejected, or if zero resources exist
-          disabled={status === "rejected" || total === 0 || isPending}
+          disabled={status === "Cancelled" || total === 0 || isPending}
           onCheckedChange={(checked) => {
             if (checked) {
-              markFullyDelivered(id);
+              markFullyDelivered({
+                orderId: Number(id),
+                receiveDate: new Date().toISOString().split("T")[0], 
+              });
             }
           }}
         />
