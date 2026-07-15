1e1524fe2d6e89f40ae82c160fc2f622730e9489
A-sleh <businessabdo9@gmail.com>
2026-07-15 20:49:48 +0300

    config the domain

--- src/config/domain.ts
+++ src/config/domain.ts
@@ -1,11 +1,12 @@
 const development_domain = 'http://aleppo-reconsruction-5th.runasp.net';
-const production_domain = 'http://vdg.runasp.net';
+const production_domain = 'http://aleppo-reconsruction-5th.runasp.net';
 
 const development_domain_api = `${development_domain}/api/`
+const production_domain_api = `${production_domain}/api/`
 // const development_domain_api = `http://localhost:3000/`
 
 
-const SERVER_URL = process.env.NODE_ENV === 'development' ? development_domain_api : production_domain;
+const SERVER_URL = process.env.NODE_ENV === 'development' ? development_domain_api : production_domain_api;
 const API_URL: string = `${SERVER_URL}/`;
 
 export { SERVER_URL, API_URL };
