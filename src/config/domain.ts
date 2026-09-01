0f62448fb9a8347f7ace20210e8352b915609fc2
A-sleh <businessabdo9@gmail.com>
2026-09-01 22:48:12 +0300

    test

--- src/config/domain.ts
+++ src/config/domain.ts
@@ -1,12 +1,14 @@
-const development_domain = 'http://aleppo-reconsruction-5th.runasp.net';
-const production_domain = 'http://aleppo-reconsruction-5th.runasp.net';
+const development_domain = "https://aleppo-reconsruction-5th.runasp.net";
+const production_domain = "https://aleppo-reconsruction-5th.runasp.net";
 
-const development_domain_api = `${development_domain}/api/`
-const production_domain_api = `${production_domain}/api/`
+const development_domain_api = `${development_domain}/api/`;
+const production_domain_api = `${production_domain}/api/`;
 // const development_domain_api = `http://localhost:3000/`
 
-
-const SERVER_URL = process.env.NODE_ENV === 'development' ? development_domain_api : production_domain_api;
+const SERVER_URL =
+  process.env.NODE_ENV === "development"
+    ? development_domain_api
+    : production_domain_api;
 const API_URL: string = `${SERVER_URL}/`;
 
-export { SERVER_URL, API_URL };
+export { API_URL, SERVER_URL };
